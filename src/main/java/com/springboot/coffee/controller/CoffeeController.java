1d860509177d3251076e30e514c1e5e44e29d355
wonil112 <nicewonil@naver.com>
2024-08-08 12:27:06 +0900

    add CoffeeController

---
+++ src/main/java/com/springboot/coffee/controller/CoffeeController.java
@@ -0,0 +1,15 @@
+package com.springboot.coffee.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/coffee")
+public class CoffeeController {
+
+    @GetMapping
+    public String getCoffee() {
+        return "Hello Coffee World!";
+    }
+}
